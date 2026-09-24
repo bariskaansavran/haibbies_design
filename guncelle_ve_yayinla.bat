f80cc97fa626bdc5e15f2dd000b2790babfb2f8b
Barış Kaan SAVRAN <baris.savran@aysademir.local>
2026-09-24 14:57:23 +0300

    Katalog Guncellemesi

---
+++ guncelle_ve_yayinla.bat
@@ -0,0 +1,14 @@
+@echo off
+echo ===================================================
+echo HAIBBIES DESIGN - KATALOG GUNCELLEME VE YAYINLAMA
+echo ===================================================
+echo.
+echo Dosyalar GitHub'a (Buluta) yukleniyor...
+git add .
+git commit -m "Katalog Guncellemesi"
+git push
+echo.
+echo ===================================================
+echo ISLEM TAMAMLANDI! Web sitesi 1-2 dakika icinde guncellenecek.
+echo ===================================================
+pause
